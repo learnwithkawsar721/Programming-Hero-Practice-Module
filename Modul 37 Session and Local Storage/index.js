a85836e1399c18c80e638a14897b0489bb89ced2
learnwithkawsar <kawsar721g@gmail.com>
2021-09-05 06:43:41 +0600

    Modiul 37

--- Modul 37 Session and Local Storage/index.js
+++ Modul 37 Session and Local Storage/index.js
@@ -1,5 +1,58 @@
+//globel Variable
+const ul = document.getElementById('products');
 const addItem = () => {
     const productField = document.getElementById('product-name');
     const product = productField.value;
-    console.log(product);
-    pr
+    if (!product) {
+        return;
+    }
+    // display item
+    displayItem(product);
+    // add to Local Storage
+    addproductToCart(product);
+    productField.value = '';
+}
+
+const displayCart = () => {
+    const cart = getCart();
+    for (const name in cart) {
+        displayItem(name);
+    }
+}
+
+const displayItem = name => {
+    const li = document.createElement('li');
+    const cart = getCart();
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    }
+
+    li.innerText = name;
+    ul.appendChild(li);
+
+}
+// getCart function
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+    cart ? cartObj = JSON.parse(cart) : cartObj = {};
+    return cartObj;
+}
+// clear Cart function 
+const clearCart = () => {
+    localStorage.removeItem('cart');
+}
+const addproductToCart = name => {
+    const cart = getCart();
+    cart[name] ? cart[name] = cart[name] + 1 : cart[name] = 1;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+// placeOrder 
+const pleceOrder = () => {
+    ul.textContent = '';
+    clearCart();
+}
+// call display Cart show 
+displayCart();
